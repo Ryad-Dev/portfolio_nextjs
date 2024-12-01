a9a9ab6dd90736fc8e677756e67147c183bd0e5e
Ryad-Dev <contactryaddev@gmail.com>
2024-12-01 23:57:33 +0100

    fix

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -15,9 +15,9 @@ const Hero: React.FC<HeroProps> = () => {
   return (
     <div className="text-center w-full h-screen flex justify-center items-center">
       <div className="bg-neutral-900 text-white w-5/6 h-5/6 flex  flex-wrap items-center justify-center rounded-3xl p-6">
-        <div className="flex flex-col lg:flex-row bg- items-center justify-evenly space-y-6 lg:space-y-0 lg:space-x-8">
+        <div className="flex flex-col lg:flex-row bg- items-center justify-evenly space-y-1 lg:space-y-2 lg:space-x-8">
           {/* Image de Profil */}
-          <div className="flex justify-center items-center w-52 h-52 lg:w-60 lg:h-60 bg-blue-700 rounded-full overflow-hidden">
+          <div className="flex justify-center items-center w-28 h-28 lg:w-60 lg:h-60 bg-blue-700 rounded-full overflow-hidden">
             {/* <img
               src="/profil.png"
               alt="photo-profil"
@@ -35,13 +35,13 @@ const Hero: React.FC<HeroProps> = () => {
 
           {/* Texte et description */}
           <div className="flex flex-col items-center lg:items-start w-full lg:w-1/2 p-4">
-            <h1 className="text-xl font-bold lg:text-2xl xl:text-2xl text-white leading-snug">
+            <h1 className="text-l font-bold lg:text-2xl xl:text-2xl text-white leading-snug">
               Hi, I&apos;m Ryad
             </h1>
-            <h2 className="mt-2 text-l font-semibold lg:text-xl text-white">
+            <h2 className="mt-2 text-sm font-semibold lg:text-xl text-white">
               Software Developer & SaaS Addict
             </h2>
-            <p className="py-4 text-xl text-gray-300 leading-snug flex justify-start">
+            <p className="py-4 text-sm lg:text-xl text-gray-300 leading-snug flex justify-start">
               I build innovative solutions using modern technologies and
               effective approaches.
             </p>
@@ -50,12 +50,12 @@ const Hero: React.FC<HeroProps> = () => {
             
 
             {/* Icônes de réseaux sociaux */}
-            <div className="flex justify-center items-start space-x-4 mt-6">
+            <div className="flex justify-center items-start space-x-4 mt-4">
               <a
                 href="https://github.com/Ryad-Dev"
                 target="_blank"
                 rel="noopener"
-                className="flex justify-center items-center h-12 w-12 bg-indigo-600 rounded-full text-white"
+                className="flex justify-center items-center h-10 w-10 bg-indigo-600 rounded-full text-white"
               >
                 <i className="fa fa-github text-4xl"></i> {/* Logo GitHub */}
               </a>
@@ -63,7 +63,7 @@ const Hero: React.FC<HeroProps> = () => {
                 href="https://instagram.com/ryad.sc"
                 target="_blank"
                 rel="noopener"
-                className="flex justify-center items-center h-12 w-12 bg-red-400 rounded-full text-white"
+                className="flex justify-center items-center h-10 w-10 bg-red-400 rounded-full text-white"
               >
                 <i className="fa fa-instagram text-4xl"></i>{" "}
                 {/* Logo Instagram */}
@@ -72,7 +72,7 @@ const Hero: React.FC<HeroProps> = () => {
                 href="https://x.com/ryad_sc"
                 target="_blank"
                 rel="noopener"
-                className="flex justify-center items-center h-12 w-12 bg-blue-400 rounded-full text-white"
+                className="flex justify-center items-center h-10 w-10 bg-blue-400 rounded-full text-white"
               >
                 <i className="fa fa-twitter text-4xl"></i> {/* Logo X */}
               </a>
